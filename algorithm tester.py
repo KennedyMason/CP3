aae707f72ef8aaf5c8b25d79af8ae60460d553fe
KennedyMason <kennedy.mason@ucas-edu.net>
2024-09-27 11:38:26 -0600

    I finished pigeonhole and started selection.

--- algorithm tester.py
+++ algorithm tester.py
@@ -6,30 +6,48 @@ data_set = data_set.split()
 for item in range(0, len(data_set)):
     data_set[item] = int(data_set[item])
 
-print(data_set)
-
 
 
 #pigeonhole sort
+def pigeonhole_sort(a):
 
-#finds the maximum, minimum, and range
-max = max(data_set)
-min = min(data_set)
-range_of_set = max - min
-sorted_set = []
+    #finds the maximum, minimum, and range
+    my_max = max(a)
+    my_min = min(a)
+    range_of_set = (my_max - my_min)+1
+    holes = [0] * range_of_set
 
-print(min)
-print(max)
+    #populate holes
+    for element in a:
+        holes[element - my_min] += 1
 
-#begins the sort
-for i in range(len(data_set)):
+    #begins the sort
+    i = 0
+    for count in range(range_of_set):
 
-    element = i + min
+        while holes[count] > 0:
+            holes[count] -= 1
+            a[i] = count + my_min
+            i += 1
+    print(a)
 
-    if i + min in data_set:
-        sorted_set.append(element)
 
-print(sorted_set)
+
+#selection sort
+def selection_sort(a):
+
+    for i in a:
+        
+        if a[i] > a[-i]:
+
+            a[i] = a[-i]
+            a[-i] = a[i]
+
+    print(a)
+
+
+selection_sort(data_set)
+
 
     
 
