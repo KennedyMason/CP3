807c851e8dee12a8edca747c032f3090c5e26517
KennedyMason <kennedy.mason@ucas-edu.net>
2024-10-15 11:18:01 -0600

    I changed the total method

---
+++ UML_notes.py
@@ -0,0 +1,30 @@
+"""
+UML: Unifed Modeling Language
+
+
+Diagram Types :
+
+[Structure]
+    - Composite:
+    - Deployment: models physical aspects of objects
+    - Package: shows packages and dependencies between them
+    - Profile
+    - Class
+    - Object: shows details of a systems state at a specific point in time
+    - Component
+
+
+[Behavioral]
+    - Activity: graphic representations of workflows
+    - Use Case
+    - State: describes system behavior
+    - Interaction+ sequence, communication, interaction overview, timing
+
+
+These can help examine the system at different angles. There are often many collaborators on a system that all care about different aspects.
+
+Class diagram example:
+https://lucid.app/lucidchart/5ccf4b6b-4558-4107-83b6-d84e846e6c2d/edit?beaconFlowId=A589F132F0145E1A&invitationId=inv_71f56c51-83f1-429d-aae4-3e620aa81894&page=0_0#
+
+"""
+
--- menu_class.py
+++ menu_class.py
@@ -1,17 +1,20 @@
+import os
 
-
+def clear():   
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
 class Order:
 
     drink_menu = {"Water" : 0.50,
-                  "Dr. Pepper" : 1.20,
-                  "Coca-Cola" : 1.20,
+                  "Dr Pepper" : 1.20,
+                  "Coca Cola" : 1.20,
                   "Sprite" : 1.00,
                   "Apple Juice" : 0.75}
     
     app_menu = {"Fries" : 2.50,
-                "Tater-Tots" : 3.25,
+                "Tater Tots" : 3.25,
                 "Breadsticks" : 3.50,
-                "Chips and Salsa" : 2.00}
+                "Chips" : 2.00}
     
     main_menu = {"Pizza" : 8.00,
                  "Burger" : 6.00,
@@ -36,40 +39,178 @@ class Order:
         self.dessert = dessert
 
     def __str__(self):
-        return f"Your order is:\n Drink: {self.drink}\n Appetizer: {self.app}\n Main Course: {self.main}\n Side 1: {self.side1}\n Side 2: {self.side2}\n Dessert: {self.dessert}"
+        return f"\nYour order is:\n Drink: {self.drink}\n Appetizer: {self.app}\n Main Course: {self.main}\n Side 1: {self.side1}\n Side 2: {self.side2}\n Dessert: {self.dessert}"
     
     
     def order_total(self):
-        drink = Order.drink_menu[self.drink.title()]
-        app = Order.app_menu[self.app.title()]
-        main = Order.main_menu[self.main.title()]
-        side1 = Order.side_menu[self.side1.title()]
-        side2 = Order.side_menu[self.side2.title()]
-        dessert = Order.dessert_menu[self.dessert.title()]
+
+        if self.drink == "None":
+            drink = 0
+        else:
+            drink = Order.drink_menu[self.drink.title()]
+        
+        if self.app == "None":
+            app = 0
+        else:
+            app = Order.app_menu[self.app.title()]
+
+        if self.main == "None":
+            main = 0
+        else:
+            main = Order.main_menu[self.main.title()]
+
+        if self.side1 == "None":
+            side1 = 0
+        else:
+            side1 = Order.side_menu[self.side1.title()]
+
+        if self.side2 == "None":
+            side2 = 0
+        else:
+            side2 = Order.side_menu[self.side2.title()]
+
+        if self.dessert == "None":
+            dessert = 0
+        else:
+            dessert = Order.dessert_menu[self.dessert.title()]
 
         total = drink + app + main + side1 + side2 + dessert
 
         return f"Your total is ${total}."
 
 
-def take_order():
+drink_choice = ""
+app_choice = ""
+main_choice = ""
+side1_choice = ""
+side2_choice = ""
+dessert_choice = ""
 
-    print("Hello! Welcome to Kennedy's Restaurant\n")
+def view_menu():
 
-    need_menu = ("Would you like to see the menu? (y/n): ")
-
-    if need_menu.lower() == "y":
-
-        print("There are five menus:\n\
+    looking = True
+    
+    print("\nThere are five menus:\n\
 a. Drinks\n\
-b. ")
+b. Appetizers\n\
+c. Main Courses\n\
+d. Sides\n\
+e. Desserts")
 
-order = Order("Water", "Fries", "Pizza", "Salad", "Baked Potato", "Ice Cream")
+    while looking == True:
+    
+        menu_type = input("\nWhich menu would you like to view? (letter): ")
+        
+        if menu_type.lower(). == "a":
+            
+            print("\nDrink menu:")
+            print("\n".join("{}\t{}".format(k, v) for k, v in Order.drink_menu.items()))
+            
+        elif menu_type.lower(). == "b":
+            
+            print("\nAppetizer menu: ")
+            print("\n".join("{}\t{}".format(k, v) for k, v in Order.app_menu.items()))
+            
+        elif menu_type.lower(). == "c":
+            
+            print("\nMain course menu: ")
+            print("\n".join("{}\t{}".format(k, v) for k, v in Order.main_menu.items()))
+            
+        elif menu_type.lower(). == "d":
+            
+            print("\nSide menu: ")
+            print("\n".join("{}\t{}".format(k, v) for k, v in Order.side_menu.items()))
+            
+        elif menu_type.lower(). == "e":
+            
+            print("\nDessert menu: ")
+            print("\n".join("{}\t{}".format(k, v) for k, v in Order.dessert_menu.items()))
+            
+        contin = input("\nWould you like to keep looking? (y/n): ")
+        
+        if contin == "n":
+            looking = False
 
+
+def take_order():
+    
+    global drink_choice
+    global app_choice
+    global main_choice
+    global side1_choice
+    global side2_choice
+    global dessert_choice
+    
+    ordered = False
+    
+    print("\nIf you do not wish to order an item, please enter 'None'.\n")
+    
+    while ordered == False:
+    
+        #choose drink
+        drink_choice = input("Enter drink choice: ")
+        
+        while drink_choice.title() not in Order.drink_menu or drink_choice.title() != "None":
+            
+            print("\nPlease enter and item from the drink menu.")
+            drink_choice = input("\nEnter drink choice: ")
+            
+            
+        #choose appetizer
+        app_choice = input("Enter appetizer choice: ")
+        
+        while app_choice.title() not in Order.app_menu or app_choice.title() != "None":
+            
+            print("\nPlease enter and item from the appetizer menu.")
+            app_choice = input("\nEnter appetizer choice: ")
+            
+        
+        #choose main course
+        main_choice = input("Enter main course choice: ")
+        
+        while main_choice.title() not in Order.main_menu or main_choice.title() != "None":
+            
+            print("\nPlease enter and item from the main course menu.")
+            main_choice = input("\nEnter main course choice: ")
+            
+        
+        #choose first side 
+        side1_choice = input("Enter first side choice: ")
+        
+        while side1_choice.title() not in Order.side_menu or _choice.title() != "None":
+            
+            print("\nPlease enter and item from the side menu.")
+            side1_choice = input("\nEnter first side choice: ")
+        
+    
+        #choose second side
+        side2_choice = input("Enter second side choice: ")
+    
+        while side2_choice.title() not in Order.side_menu:
+        
+            print("\nPlease enter and item from the side menu.")
+            side2_choice = input("\nEnter second side choice: ")
+        
+        #choose dessert
+        dessert_choice = input("Enter dessert choice: ")
+    
+        while dessert_choice.title() not in Order.dessert_menu:
+        
+            print("\nPlease enter and item from the dessert menu.")
+            dessert_choice = input("\nEnter dessert choice: ")
+            
+        complete = input("Complete your order? (y/n): ")
+        
+        if complete == "y":
+            ordered = True
+
+see_menu = input("Welcome to Kennedy's Restaurant! Would you like to see a menu? (y/n): ")
+if see_menu == "y":
+    view_menu()
+
+take_order()
+
+order = Order(drink_choice, app_choice, main_choice, side1_choice, side2_choice, dessert_choice)
+
+print(order.__str__())
 print(order.order_total())
-
-
-
-
-
-
