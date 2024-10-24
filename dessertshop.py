8bfc96e3ea638fdcbd19cafcacac58dd691c2959
KennedyMason <kennedy.mason@ucas-edu.net>
2024-10-24 11:35:38 -0600

    yuh

--- dessertshop.py
+++ dessertshop.py
@@ -12,7 +12,7 @@ class DessertItem:
 
 # candy class
 class Candy(DessertItem):
-
+    
     def __init__(self, name, candy_weight, price_per_pound):
         super().__init__(name)
         self.candy_weight = candy_weight
@@ -22,6 +22,10 @@ class Candy(DessertItem):
 
 # cookie class
 class Cookie(DessertItem):
+    
+    cookie_menu = {"Chocalate Chip" : 18.00,
+                   "Snickerdoodle" : 24.00,
+                    "Oatmeal" : 12.00}
     
     def __init__(self, name, cookie_quantity, price_per_dozen):
         super().__init__(name) 
@@ -49,5 +53,40 @@ class Sundae(IceCream):
         self.topping_price = topping_price
 
 
-    
 
+
+# class that makes order
+class Order:
+
+    def __init__(self, order = []):
+        self.order = order
+
+    def __str__(self):
+        for item in self.order:
+            return f"{item}"
+        
+    def add(self, item):
+        self.order.append(item)
+    
+    def total_items(self):
+        total = len(self.order)
+        return f"{total}"
+
+
+
+def main():
+
+    order = Order([])
+
+    order.add(Candy("Gummy Bears", 5, 2.00))
+    order.add(Cookie("Chocolate Chip", 12, 18.00))
+    order.add(IceCream("Vanilla", 2, 0.50))
+    order.add(Sundae("Chocolate", 3, 1.00, "Syrup", 1.00))
+
+    item_amount = order.total_items
+
+    print(order)
+    print(f"Total number of items: {item_amount}")
+
+
+main()
--- testing/test_dessert_class.py
+++ testing/test_dessert_class.py
@@ -10,3 +10,32 @@ def test_string():
     test_item = DessertItem("Cookie")
     test_string = test_item.__str__()
     assert test_string == "The dessert is Cookie."
+
+def test_candy():
+    test_item = Candy("Lollipop", 20, 5.00)
+    assert test_item.name == "Lollipop"
+    assert test_item.candy_weight == 20
+    assert test_item.price_per_pound == 5.00
+  
+def test_cookie():
+    test_item = Cookie("Snickerdoodle", 12, 10.00)
+    assert test_item.name == "Snickerdoodle"
+    assert test_item.cookie_quantity == 12
+    assert test_item.price_per_dozen == 10.00
+
+def test_ice_cream():
+    test_item = IceCream("Vanilla", 3, 2.00)
+    assert test_item.name == "Vanilla"
+    assert test_item.scoop_count == 3
+    assert test_item.price_per_scoop == 2.00
+
+def test_sundae():
+    test_item = Sundae("Vanilla", 3, 2.00, "Cherry", 0.50)
+    assert test_item.name == "Vanilla"
+    assert test_item.scoop_count == 3
+    assert test_item.price_per_scoop == 2.00
+    assert test_item.topping_name == "Cherry"
+    assert test_item.topping_price == 0.50
+
+
+
