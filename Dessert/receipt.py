938e8135bb7a078d47eed7503e41bb81d9d29aad
KennedyMason <kennedy.mason@ucas-edu.net>
2024-12-14 18:38:13 -0700

    dessert shop 3 (second attempt)

--- Dessert/receipt.py
+++ Dessert/receipt.py
@@ -3,7 +3,9 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4 
 from reportlab.lib.styles import getSampleStyleSheet 
 
-def make_receipt(data,out_file_name):     
+def make_receipt(data,out_file_name):
+    # imports module     
+    # data which we are going to display as tables 
     """
     DATA = [ 
     [ "Date" , "Name", "Subscription", "Price (Rs.)" ], 
@@ -20,17 +22,26 @@ def make_receipt(data,out_file_name):
     ] 
     """
     
+    # creating a Base Document Template of page size A4 
     pdf = SimpleDocTemplate( "receipt.pdf" , pagesize = A4 ) 
     
+    # standard stylesheet defined within reportlab itself 
     styles = getSampleStyleSheet() 
     
-    title_style = styles[ "Heading1" ]  
+    # fetching the style of Top level heading (Heading1) 
+    title_style = styles[ "Heading1" ] 
     
+    # 0: left, 1: center, 2: right 
     title_style.alignment = 1
-   
-    title = Paragraph( "Receipt" , title_style ) 
     
-
+    # creating the paragraph with  
+    # the heading text and passing the styles of it 
+    title = Paragraph( "GeeksforGeeks" , title_style ) 
+    
+    # creates a Table Style object and in it, 
+    # defines the styles row wise 
+    # the tuples which look like coordinates  
+    # are nothing but rows and columns 
     style = TableStyle( 
         [ 
             ( "BOX" , ( 0, 0 ), ( -1, -1 ), 1 , colors.black ), 
@@ -45,5 +56,6 @@ def make_receipt(data,out_file_name):
     # creates a table object and passes the style to it 
     table = Table( data , style = style ) 
     
-   
+    # final step which builds the 
+    # actual pdf putting together all the elements 
     print(pdf.build([ title , table ])) 
